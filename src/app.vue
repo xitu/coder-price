5312c86535650c6052280a5da89f84a140f341b9
kalasoo <ym.kalasoo@gmail.com>
2015-02-02 15:58:28 +0800

    components

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<style lang="stylus">
+</style>
+
+
+<template lang="jade">
+header(v-with="title")
+questions
+footer(v-with="price")
+</template>
+
+
+<script lang="coffee">
+module.exports =
+    data:
+        title: 'TITLE'
+        totalPrice: 0
+        questions: require './questions.json'
+    components:
+        header    : require './components/header.vue'
+        questions : require './components/questions.vue'
+        footer    : require './components/footer.vue'
+    methods:
+        setTitle: (title) ->
+            @title = title
+        addPrice: (price) ->
+            @totalPrice += price
+        subscribePrice: (price) ->
+            @totalPrice -= price
+</script>
