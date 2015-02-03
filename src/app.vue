0619ad58059377edcb64c183217fe1aae33e40b7
kalasoo <ym.kalasoo@gmail.com>
2015-02-03 12:06:29 +0800

    title changed in wechat

--- src/app.vue
+++ src/app.vue
@@ -100,7 +100,6 @@ section.share.text-center(v-if="!currentQuestion()")
     p 这个程序员需要 ￥{{totalPrice}} 才能招的起！
     p
         span(v-if="inWechat" style="color: red;") 点击右上角分享到微信
-        - var desc="招一个好程序猿要 @稀土圈"
         span(v-if="!inWechat")
             a(v-attr="href: weiboShareUrl()", target='_blank') 分享到微博
     p.promo
@@ -108,7 +107,7 @@ section.share.text-center(v-if="!currentQuestion()")
             a(href='https://xitu.io/') 稀土
             | 可以找到比这更棒的程序猿哦！
 
-footer(v-if="currentQuestion()")
+footer
     p ￥{{showPrice}}
 </template>
 
@@ -150,6 +149,8 @@ module.exports =
                 toPrice = countPrice()
                 showPrice(@totalPrice, toPrice)
                 @totalPrice = toPrice
+                if @currentQuestionIndex is @questions.length
+                    document.title = "我招一个好程序猿要￥#{@totalPrice}，看看你需要多少钱的程序猿？"
         enableNextQuestion: () ->
             @questions[@currentQuestionIndex].selected isnt null
         weiboShareUrl: () ->
